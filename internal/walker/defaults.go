befddd64ba2f7e4ee9efc1ae9fa4e4e923336bfb
Muhamet Smaili <muhamet5@msn.com>
2025-11-30 19:48:13 +0200

    chore: more default ignores for safer renaming, there is an option for skiping them --no-defaults

--- internal/walker/defaults.go
+++ internal/walker/defaults.go
@@ -1,6 +1,29 @@
 package walker
 
-// TODO: check and add more sane defautls to ignore
 var DefaultIgnorePatterns = []string{
+	// Version control
 	".git",
+	".svn",
+	".hg",
+
+	// Dependencies
+	"node_modules",
+	"vendor",
+
+	// Build outputs
+	"dist",
+	"build",
+	"target",
+
+	// Caches
+	"__pycache__",
+	".cache",
+
+	// Virtual environments
+	".venv",
+	"venv",
+
+	// OS files
+	".DS_Store",
+	"Thumbs.db",
 }
